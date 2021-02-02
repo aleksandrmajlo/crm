20cd1b888825c63b6c652d2ded0cb5dfe230f7fb
aleksandrmajlo <aleksandrmajlo@gmail.com>
2021-02-02 16:23:26 +0200

    02.02.21 search archive

--- resources/views/archive/search.blade.php
+++ resources/views/archive/search.blade.php
@@ -3,7 +3,7 @@
 </h2>
 <div class="row justify-content-center mb-5">
     <div class=" col col-md-8">
-        <form class=" ">
+        <form method="get" action="/archives">
             <div class="input-group mb-2">
                 <input type="search" name="id" class="form-control " value="{{app('request')->input('id')}}"
                        placeholder="Search by ID">
@@ -20,7 +20,7 @@
 </h2>
 <div class="row justify-content-center mb-5">
     <div class=" col col-md-8">
-        <form class=" ">
+        <form method="get" action="/archives">
             <div class="input-group mb-2">
                 <input type="search" name="serial" class="form-control " value="{{app('request')->input('serial')}}"
                        placeholder="Search by Serial">
